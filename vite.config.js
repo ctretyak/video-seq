a07fd41d62970e24aed54f23cd00d50fff68e135
ctretyak <ctretyak@gmail.com>
2024-08-27 13:36:58 +0300

    fix: ios

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ export default {
   base: '/video-seq',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        callback: 'callback.html',
+      },
+    },
   }
 }
