7b01b20f7ada3f2b27a3ff7eea1774330b1827c2
ctretyak <ctretyak@gmail.com>
2024-08-27 12:12:43 +0300

    fix: ios

--- src/video-tree/tree.ts
+++ src/video-tree/tree.ts
@@ -89,23 +89,25 @@ export class VideoTree {
     const video = this.getVideo();
     video.pause();
     video.currentTime = 0;
-    if (this.iOS()) {
-      /* you have to call load for ios devices
-       * https://stackoverflow.com/questions/49792768/js-html5-audio-why-is-canplaythrough-not-fired-on-ios-safari
-       * */
-      video.load();
-      return new Promise<void>((resolve) => {
-        const listener = () => {
-          video.removeEventListener("loadeddata", listener);
-          video.pause();
-          video.currentTime = 0;
-          video.play();
-          video.pause();
-          resolve();
-        }
-        video.addEventListener("loadeddata", listener)
-      })
-    }
+    video.play();
+    video.pause();
+    // if (this.iOS()) {
+    //   /* you have to call load for ios devices
+    //    * https://stackoverflow.com/questions/49792768/js-html5-audio-why-is-canplaythrough-not-fired-on-ios-safari
+    //    * */
+    //   video.load();
+    //   return new Promise<void>((resolve) => {
+    //     const listener = () => {
+    //       video.removeEventListener("loadeddata", listener);
+    //       video.pause();
+    //       video.currentTime = 0;
+    //       video.play();
+    //       video.pause();
+    //       resolve();
+    //     }
+    //     video.addEventListener("loadeddata", listener)
+    //   })
+    // }
   }
 
   play() {
