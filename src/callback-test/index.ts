bd59021cee47deac744cce6d23b0d8e81dcaa08a
ctretyak <ctretyak@gmail.com>
2024-08-27 16:06:33 +0300

    fix: ios

--- src/callback-test/index.ts
+++ src/callback-test/index.ts
@@ -1,4 +1,4 @@
-const [player, canvas, play, pause, seek0, playerInvisible, playerDisplayNone, playerLeft, playerReset] = [
+const [player, canvas, play, pause, seek0, playerInvisible, playerDisplayNone, playerLeft, playerReset, playerOverlay] = [
   document.getElementById("player") as HTMLVideoElement,
   document.getElementById("canvas") as HTMLCanvasElement,
   document.getElementById("play") as HTMLButtonElement,
@@ -8,10 +8,12 @@ const [player, canvas, play, pause, seek0, playerInvisible, playerDisplayNone, p
   document.getElementById("player-display-none") as HTMLButtonElement,
   document.getElementById("player-left") as HTMLButtonElement,
   document.getElementById("player-reset") as HTMLButtonElement,
+  document.getElementById("player-overlay") as HTMLButtonElement,
 ]
 
 const video = document.createElement("video");
 video.src = "https://ctretyak.github.io/video-test/monster_1.mp4.mp4";
+// video.src = "https://ctretyak.github.io/video-test/L1.mp4.mp4";
 video.controls = true;
 video.muted = true;
 video.playsInline = true;
@@ -51,6 +53,14 @@ playerReset.onclick = () => {
   player.style.left = "initial";
 }
 
+playerOverlay.onclick = () => {
+  player.style.zIndex = "99";
+  canvas.style.zIndex = "100";
+  player.style.position = "relative";
+  canvas.style.position = "absolute";
+  canvas.style.top = "0px";
+}
+
 // let paintCount = 0;
 let startTime = 0.0;
 
@@ -59,7 +69,9 @@ const updateCanvas = (now: any) => {
   if (startTime === 0.0) {
     startTime = now;
   }
+  console.log(video.videoWidth, video.videoHeight)
 
+  debugger
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   // const elapsed = (now - startTime) / 1000.0;
